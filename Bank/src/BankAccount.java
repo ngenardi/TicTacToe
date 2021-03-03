80b030904af922ff6569702abbc19ebc472b3242
neil_swagger <neil_swagger@localhost>
2021-03-03 12:29:18 -0500

    Second version

--- Bank/src/BankAccount.java
+++ Bank/src/BankAccount.java
@@ -6,13 +6,17 @@ public class BankAccount
 {
 	//Instance variable
 	private double balance;
+double Interest;
 
+	public String getBalance;
+	
    /**
       Constructs a bank account with zero balance.
    */
-   public BankAccount(double amount)
+   public BankAccount(double amount, double totalInterest)
    {
       balance = amount;
+      Interest = totalInterest;
    }
 
    /**
@@ -31,12 +35,28 @@ public class BankAccount
    */
    public void withdraw(double amount)
    {
-      balance = balance - amount;
+      if (balance < amount) { System.out.println("Insufficent Funds to support withdrawl");}
+      else {balance = balance - amount; }
    }
    /**
       Gets the current balance of this bank account.
+ * @return 
       @return the current balance
    */
+   public double calcInterest(double b, double r, int n, int t) {
+	   double newBalance=(b*Math.pow(1+(r/n),n*t))-b;
+  return newBalance; 
+		  }
+   public void InterestMeth(double interestRate) {
+	   
+	   Interest= balance*interestRate;
+	   balance=balance+ Interest;
+   }
+   public double getInterest()
+   {
+      return Interest;
+   }
+   
    public double getBalance()
    {
       return balance;
--- Bank/src/BankAccountTester.java
+++ Bank/src/BankAccountTester.java
@@ -1,29 +1,56 @@
 import java.util.Scanner;
+
 public class BankAccountTester {
 
-	
 	public static void main(String[] args) {
-Scanner in = new Scanner(System.in);
-System.out.println("Enter Starting Balance for your account");
-double startbal= in.nextDouble();
- BankAccount myBankAccount= new BankAccount(startbal);
- 
- double startBal=in.nextDouble();
- System.out.println("Enter how much you would like to withdraw: ");
- double withdrawamount= in.nextDouble();
- myBankAccount.withdraw(withdrawamount);
- 
- double startBal1=in.nextDouble();
- System.out.println("Enter how much you would like to deposit: ");
- double depositamount= in.nextDouble();
- myBankAccount.deposit(depositamount);
- 
- System.out.println("availible balance" + myBankAccount);
- double startbal1= in.nextDouble();
-System.out.print(myBankAccount.getBalance());
- 
-		
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter Starting Balance for your account: ");
+		double startbal = in.nextDouble();
+		System.out.print("enter the interest rate for this account: ");
+		double Interest = in.nextDouble();
+		BankAccount myBankAccount = new BankAccount(startbal, Interest);
+		System.out.println("    Create a new Bank Account " + startbal + " balance and interest rate " + Interest);
+
+		System.out.print("Enter how much you would like to deposit: ");
+		double depositamount = in.nextDouble();
+		System.out.println("    Deposited" + depositamount);
+		myBankAccount.deposit(depositamount);
+
+		System.out.print("Enter how much you would like to withdraw: ");
+		double withdrawamount = in.nextDouble();
+		double BankAccountbalance = myBankAccount.getBalance();
+		if (BankAccountbalance > withdrawamount) {
+			myBankAccount.withdraw(withdrawamount);
+			// BankAccountbalance = BankAccountbalance - withdrawamount;
+			System.out.println("    Withdrew:" + withdrawamount);
+		} else {
+			System.out.println("    Insufficent Funds to Support Withdrawl");
+		}
+
+		System.out.print("Enter how much you would like to withdraw: ");
+		double nextwithdrawamount = in.nextDouble();
+		if (BankAccountbalance > nextwithdrawamount) {
+			myBankAccount.withdraw(nextwithdrawamount);
+			BankAccountbalance = BankAccountbalance - nextwithdrawamount;
+			System.out.println("    Withdrew: " + nextwithdrawamount);
+		} else {
+			System.out.println("    Insufficent Funds to Support Withdrawl");
+		}
+		String a = "Yes";
+		String b = "yes";
+		System.out.print("If you want me to generate interest, enter 'Yes': ");
+		String response = in.next();
+		if (response.equals(a) || response.equals(b)) {
+			double BankAccountInterest = myBankAccount.calcInterest(BankAccountbalance, Interest, 1, 1);
+			System.out.println("    Interest: " + BankAccountInterest);
+			BankAccountbalance = BankAccountbalance + BankAccountInterest;
+		} else {
+			System.out.println("This is your account without Interest" + myBankAccount.getBalance);
+			System.exit(0);
+		}
+
+		System.out.println("This is your total Balance: " + (BankAccountbalance));
+
 	}
 
 }
