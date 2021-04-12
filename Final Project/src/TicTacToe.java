4f0a4d4e23ffbaaa0befd8cfef5192a84b96ab23
cheemapurp <cheemapurp@localhost>
2021-04-12 18:44:56 -0400

    Not final

--- Final Project/src/TicTacToe.java
+++ Final Project/src/TicTacToe.java
@@ -75,25 +75,77 @@ public class TicTacToe extends JPanel implements ActionListener {
 			return false;
 		}
 	}
+	
+	int playero;
+	int playerx;
+		
+	static int[] openPositions = {0,1,2,3,4,5,6,7,8};
+	static int[] xPositions = {9,9,9,9,9,9,9,9,9};
+	static int[] oPositions = {9,9,9,9,9,9,9,9,9};
+	    
+	static boolean oMove(int position, int turn)
+	{
+		int playerTurn = turn/2+1;
+	    for(int i=0; i<9;i++)
+	    {
+	    	if (openPositions[i] == position)
+	    	{
+	    		oPositions[playerTurn-1] = position;
+	            openPositions[i] = 9;
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	
+	static boolean xMove(int position, int turn)
+	{
+		int playerTurn = turn/2;
+		for(int i=0; i<9;i++)
+		{
+			if (openPositions[i] == position)
+			{
+				xPositions[playerTurn-1] = position;
+	            openPositions[i] = 9;
+	            return true;
+	        }
+	    }
+		return false;
+	}
 
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e)
+	{
 
 		JButton buttonClicked = (JButton) e.getSource();
 
-		if (alt % 2 == 0) {
+		if (alt % 2 == 0)
+		{
 			buttonClicked.setText("X");
-		} else {
+		}
+		else
+		{
 			buttonClicked.setText("O");
+		}
+		
+		if (xMove())
+		{
+			
+		}
+		if(oMove())
+		{
+			
 		}
 
 		alt++;
 
-		if (detectWin(new int[9]) == true) {
+		if (detectWin(new int[9]) == true)
+		{
 			JOptionPane.showConfirmDialog(null, "Game Over. \n Goodbye!");
 			buttonsReset();
 
 		}
-		if (detectTie(new int[9]) == true) {
+		if (detectTie(new int[9]) == true)
+		{
 			JOptionPane.showConfirmDialog(null, "Tie. \n Goodbye!");
 			buttonsReset();
 		}
